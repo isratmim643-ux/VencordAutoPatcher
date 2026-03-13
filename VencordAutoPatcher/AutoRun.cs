1147cb455a304a82e5ec5c0481e7269fe679e298
esu <meow@kasianiezyje.ru>
2026-03-13 21:33:43 +0100

    source code upload

---
+++ VencordAutoPatcher/AutoRun.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VencordAutoPatcher
+{
+    internal class AutoRun
+    {
+        public static void AddToStartup()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            key.SetValue("Vencord Auto Patcher", Assembly.GetExecutingAssembly().Location.Replace(".dll", ".exe"));
+            key.Close();
+            ConsoleUtilities.Log("Successfully added VAP to Startup");
+        }
+    }
+}
